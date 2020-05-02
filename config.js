7ac2573c48e442e77931f37aa3d0ee9552d97a0f
elliotconnor <54572856+elliotconnor@users.noreply.github.com>
2020-05-02 11:26:33 +1000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,355 @@
+var CONFIG = {
+ "data": {
+  "_lastModified": 1588364745817,
+  "hasNameColumn": false,
+  "lastModified": 1588364745817,
+  "nameColumnPosition": 0,
+  "noCache": true,
+  "path": "./data/IUCN v. Dev Compressed.csv",
+  "reader": "ext-csv",
+  "sheet": "",
+  "timeInColumns": false,
+  "ddfPath": "./data/IUCN v. Dev Compressed.csv"
+ },
+ "locale": {
+  "filePath": "assets/translation/",
+  "id": "en"
+ },
+ "state": {
+  "entities": {
+   "autoconfig": {
+    "excludeIDs": [
+     "tag"
+    ],
+    "type": "entity_domain"
+   },
+   "dim": "Country",
+   "filter": {},
+   "show": {},
+   "showFallback": {},
+   "showItemsMaxCount": null,
+   "skipFilter": false
+  },
+  "entities_colorlegend": {
+   "autoconfig": {
+    "excludeIDs": [
+     "tag"
+    ],
+    "type": "entity_domain"
+   },
+   "dim": "Country",
+   "filter": {},
+   "show": {},
+   "showFallback": {},
+   "showItemsMaxCount": null,
+   "skipFilter": false
+  },
+  "marker": {
+   "allowSelectMultiple": true,
+   "axis_x": {
+    "allow": {
+     "scales": [
+      "linear",
+      "log",
+      "genericLog",
+      "time",
+      "pow"
+     ]
+    },
+    "autoconfig": {
+     "index": 0,
+     "type": "measure"
+    },
+    "data": "data",
+    "domainMax": null,
+    "domainMin": null,
+    "fixBaseline": null,
+    "scaleType": "linear",
+    "spaceRef": null,
+    "syncModels": [],
+    "use": "indicator",
+    "which": "% Gov Expenditure on Military c.2018",
+    "zoomedMax": null,
+    "zoomedMin": null
+   },
+   "axis_y": {
+    "allow": {
+     "scales": [
+      "linear",
+      "log",
+      "genericLog",
+      "time",
+      "pow"
+     ]
+    },
+    "autoconfig": {
+     "index": 1,
+     "type": "measure"
+    },
+    "data": "data",
+    "domainMax": null,
+    "domainMin": null,
+    "fixBaseline": null,
+    "scaleType": "linear",
+    "spaceRef": null,
+    "syncModels": [],
+    "use": "indicator",
+    "which": "Women's share of parliamentary seats in upper house or senate (%) c.1999-2007 IPU",
+    "zoomedMax": null,
+    "zoomedMin": null
+   },
+   "color": {
+    "allow": {
+     "scales": [
+      "linear",
+      "log",
+      "genericLog",
+      "time",
+      "pow",
+      "ordinal"
+     ]
+    },
+    "autoconfig": {},
+    "data": "data",
+    "palette": {
+     "_default": "#ffb600"
+    },
+    "paletteLabels": null,
+    "scaleType": "ordinal",
+    "syncModels": [
+     "marker_colorlegend"
+    ],
+    "use": "constant",
+    "which": "_default"
+   },
+   "highlight": [],
+   "label": {
+    "autoconfig": {
+     "includeOnlyIDs": [
+      "name"
+     ],
+     "type": "string"
+    },
+    "data": "data",
+    "scaleType": "ordinal",
+    "syncModels": [],
+    "use": "property",
+    "which": "Country"
+   },
+   "limit": 5000,
+   "opacityHighlightDim": 0.1,
+   "opacityRegular": 1,
+   "opacitySelectDim": 0.3,
+   "select": [],
+   "size": {
+    "allow": {
+     "scales": [
+      "ordinal",
+      "linear",
+      "log",
+      "genericLog",
+      "pow"
+     ]
+    },
+    "autoconfig": {
+     "index": 2,
+     "type": "measure"
+    },
+    "data": "data",
+    "domainMax": null,
+    "domainMin": null,
+    "extent": [
+     0,
+     0.85
+    ],
+    "fixBaseline": 0,
+    "scaleType": "linear",
+    "spaceRef": null,
+    "syncModels": [],
+    "use": "indicator",
+    "which": "GDP Per Capita",
+    "zoomedMax": null,
+    "zoomedMin": null
+   },
+   "size_label": {
+    "_important": false,
+    "allow": {
+     "names": [
+      "_default"
+     ],
+     "scales": [
+      "ordinal",
+      "linear",
+      "log",
+      "genericLog",
+      "pow"
+     ]
+    },
+    "data": "data",
+    "domainMax": null,
+    "domainMin": null,
+    "extent": [
+     0,
+     0.33
+    ],
+    "fixBaseline": 0,
+    "scaleType": "ordinal",
+    "syncModels": [],
+    "use": "constant",
+    "which": "_default",
+    "zoomedMax": null,
+    "zoomedMin": null
+   },
+   "space": [
+    "entities",
+    "time"
+   ],
+   "superHighlight": []
+  },
+  "marker_colorlegend": {
+   "allowSelectMultiple": true,
+   "highlight": [],
+   "hook_geoshape": {
+    "data": "data",
+    "syncModels": [],
+    "use": "property",
+    "which": "shape_lores_svg"
+   },
+   "hook_rank": {
+    "data": "data",
+    "syncModels": [],
+    "use": "property",
+    "which": "rank"
+   },
+   "label": {
+    "data": "data",
+    "syncModels": [],
+    "use": "property",
+    "which": "name"
+   },
+   "limit": 1000,
+   "opacityHighlightDim": 0.1,
+   "opacityRegular": 1,
+   "opacitySelectDim": 0.3,
+   "select": [],
+   "space": [
+    "entities_colorlegend"
+   ],
+   "superHighlight": []
+  },
+  "time": {
+   "autoconfig": {
+    "type": "time"
+   },
+   "delay": 150,
+   "delayThresholdX2": 90,
+   "delayThresholdX4": 45,
+   "dim": "Year",
+   "end": "2020",
+   "endBeforeForecast": "2020",
+   "endOrigin": null,
+   "endSelected": "2020",
+   "format": {
+    "data": null,
+    "ui": null
+   },
+   "immediatePlay": true,
+   "loop": false,
+   "offset": 0,
+   "pauseBeforeForecast": true,
+   "playable": true,
+   "playing": false,
+   "record": false,
+   "round": "round",
+   "showForecast": true,
+   "start": "2020",
+   "startOrigin": null,
+   "startSelected": "2020",
+   "step": 1,
+   "unit": "year",
+   "value": "2020"
+  }
+ },
+ "ui": {
+  "adaptMinMaxZoom": false,
+  "buttons": [
+   "colors",
+   "find",
+   "zoom",
+   "trails",
+   "lock",
+   "moreoptions",
+   "presentation",
+   "sidebarcollapse",
+   "fullscreen"
+  ],
+  "chart": {
+   "decorations": {
+    "enabled": true,
+    "xAxisGroups": null
+   },
+   "labels": {
+    "dragging": true,
+    "removeLabelBox": false
+   },
+   "lockNonSelected": 0,
+   "margin": {
+    "left": 89.442,
+    "top": 0
+   },
+   "showForecastOverlay": false,
+   "superhighlightOnMinimapHover": true,
+   "trails": true,
+   "whenHovering": {
+    "higlightValueX": true,
+    "higlightValueY": true,
+    "showProjectionLineX": true,
+    "showProjectionLineY": true
+   }
+  },
+  "cursorMode": "arrow",
+  "datawarning": {
+   "doubtDomain": [],
+   "doubtRange": []
+  },
+  "dialogs": {
+   "dialog": {
+    "find": {
+     "enableSelectShowSwitch": false
+    }
+   },
+   "moreoptions": [
+    "opacity",
+    "speed",
+    "axes",
+    "size",
+    "colors",
+    "label",
+    "zoom",
+    "presentation",
+    "technical",
+    "about"
+   ],
+   "popup": [
+    "colors",
+    "find",
+    "size",
+    "zoom",
+    "moreoptions"
+   ],
+   "sidebar": [
+    "colors",
+    "find",
+    "size",
+    "zoom"
+   ]
+  },
+  "panWithArrow": false,
+  "presentation": false,
+  "show_ticks": true,
+  "sidebarCollapse": false,
+  "splash": false,
+  "zoomOnScrolling": false
+ },
+ "chartType": "BubbleChart"
+};
